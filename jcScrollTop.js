6f84ccee050974e2fc23653847912f5fb26f7f84
Jaspreet Chahal <jaspreetchahal.org@gmail.com>
2012-10-27 07:23:10 +1100

    zIndex now is a configurable property

--- jcScrollTop.js
+++ jcScrollTop.js
@@ -13,6 +13,7 @@ jQuery.fn.jcScrollTop = function(settings) {
            fontWeight:'bold',
            textPadding:'5px',
            containerWidth:'120px',
+		   zIndex:'1',
            containerBorder:'2px solid #960404',
            borderRadius:'10px 10px 0px 0px',           
            callback:function(){
@@ -21,7 +22,7 @@ jQuery.fn.jcScrollTop = function(settings) {
     },settings);
       
       $cur = this;	
-      jQuery("body").append("<div id='" + settings.scrollElementId + "' style='z-index:99999 !important;background:" + settings.backgroundColor + ";border:" + settings.containerBorder + ";border-radius:" + settings.borderRadius + ";position:fixed;bottom:-2px;width:" + settings.containerWidth + ";font-weight:" + settings.fontWeight + ";color:" + settings.foreColor + ";text-align:center;font-family:" + settings.fontFamily + ";font-size:" + settings.fontSize + ";cursor:pointer;padding:" + settings.textPadding + "'>" + settings.scrollText + "</div>");
+      jQuery("body").append("<div id='" + settings.scrollElementId + "' style='z-index: "+ settings.zIndex + ";background:" + settings.backgroundColor + ";border:" + settings.containerBorder + ";border-radius:" + settings.borderRadius + ";position:fixed;bottom:-2px;width:" + settings.containerWidth + ";font-weight:" + settings.fontWeight + ";color:" + settings.foreColor + ";text-align:center;font-family:" + settings.fontFamily + ";font-size:" + settings.fontSize + ";cursor:pointer;padding:" + settings.textPadding + "'>" + settings.scrollText + "</div>");
       var jcreposition = function() {
 			  var windowCenter = Math.max(0, ((jQuery(window).width() - $cur.outerWidth()) / 2) + jQuery(window).scrollLeft() - (parseInt(settings.containerWidth.replace('px',''))/2)) -  parseInt(settings.textPadding.replace('px','')) + "px";	
 			  if(settings.position == 'left') 
